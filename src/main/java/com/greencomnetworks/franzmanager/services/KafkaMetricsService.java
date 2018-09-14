075326097f5964b7f9cb2f42accad14b0172f2e6
Anthony <anthony.sarais@greencom-networks.com>
2018-09-14 15:24:55 +0200

    Api doesn't die anymore when it doesn't find the asked metric. It also try to reconnect to jmx when the connection has been lost

--- src/main/java/com/greencomnetworks/franzmanager/services/KafkaMetricsService.java
+++ src/main/java/com/greencomnetworks/franzmanager/services/KafkaMetricsService.java
@@ -1,5 +1,6 @@
 package com.greencomnetworks.franzmanager.services;
 
+import com.greencomnetworks.franzmanager.entities.Cluster;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,33 +16,56 @@ public class KafkaMetricsService {
     private static final Logger logger = LoggerFactory.getLogger(KafkaMetricsService.class);
     private static HashMap<String, HashMap<String, MBeanServerConnection>> mBeanServerConnections = new HashMap<>();
 
-    public static void init(){
-        ConstantsService.clusters.forEach(cluster -> {
-            mBeanServerConnections.put(cluster.name, new HashMap<>());
-
-            for (String url : cluster.jmxConnectString.split(",")) {
-                try {
-                    JMXServiceURL jmxUrl = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://" + url + "/jmxrmi");
-                    JMXConnector jmxc = JMXConnectorFactory.connect(jmxUrl, null);
-                    mBeanServerConnections.get(cluster.name).put(url.split(":")[0], jmxc.getMBeanServerConnection());
-                } catch (MalformedURLException e) {
-                    throw new RuntimeException("The following url has a bad format : " + url, e);
-                } catch (IOException e) {
-                    logger.error("Cannot connect to the following url '{}': {}", url, e.getMessage());
-                    try {
-                        Thread.sleep(10000);
-                    } catch (InterruptedException e1) {
-                        // don't care.
-                    }
-                }
-            }
-        });
+    public static void init() {
+        new Thread(new JmxConnectivityCheck(), "JmxConnectivityCheck").start();
     }
 
-    public static HashMap<String, MBeanServerConnection> getMBeanServerConnections(String clusterId){
-        if(clusterId == null){
+    public static HashMap<String, MBeanServerConnection> getMBeanServerConnections(String clusterId) {
+        if (clusterId == null) {
             clusterId = "Default";
         }
         return mBeanServerConnections.get(clusterId);
+    }
+
+
+    private static class JmxConnectivityCheck implements Runnable {
+        public void run() {
+            while (true) {
+                try {
+                    ConstantsService.clusters.forEach(cluster -> {
+                        mBeanServerConnections.put(cluster.name, new HashMap<>());
+                        for (String url : cluster.jmxConnectString.split(",")) {
+                            HashMap<String, MBeanServerConnection> mbsc = mBeanServerConnections.get(cluster.name);
+                            if (mbsc.get(url.split(":")[0]) == null
+                                /*|| mBeanServerConnections.get(url.split(":")[0]) == null*/) {
+                                connectJmx(cluster, url);
+                            } else {
+                                logger.warn(mbsc.get(url.split(":")[0]).toString());
+                            }
+                        }
+                    });
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private static void connectJmx(Cluster cluster, String url) {
+            try {
+                JMXServiceURL jmxUrl = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://" + url + "/jmxrmi");
+                JMXConnector jmxc = JMXConnectorFactory.connect(jmxUrl, null);
+                mBeanServerConnections.get(cluster.name).put(url.split(":")[0], jmxc.getMBeanServerConnection());
+            } catch (MalformedURLException e) {
+                throw new RuntimeException("The following url has a bad format : " + url, e);
+            } catch (IOException e) {
+                logger.error("Cannot connect to the following url '{}': {}", url, e.getMessage());
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e1) {
+                    // don't care.
+                }
+            }
+        }
     }
 }
